985b67b0e4b823bdbce40b5c2af438c2f440790d
Léo KADEM-SAKASEGAWA <leo@nadir-and-megumis-ipad.home>
2021-06-20 20:29:13 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,18 @@ export default function Home() {
         <h3>Bienvenue sur notre site internet !</h3>
         <Image src="/background.jpg" width="500px" height="300px" />
         <p>Nous sommes une entreprise qui a un institut japonais, un service informatique
-          et une école de piano et de solfège.
+          et une école de piano et de solfège. </p>
+
+        <p> Nous sommes une entreprise qui est très à
+          l'écoute et très patiente. Assistance Pédagogie est créée depuis 2019 par Léo KADEM-SAKASEGAWA.
+          Notre toute première agence a été fondée en 2019 à FONTENAY-SOUS-BOIS.
         </p>
+        <hr />
+        Vous souhaitez faire partie de l'académie AAP-fr ?
+        <a href="https://docs.google.com/forms/d/1K8jZMQlJG3ecI3CI67iOi6O1P1VNxLKfq_1pttIx9_c/prefill">
+          Contactez nous
+        </a>
+
       </main>
 
       <footer className={styles.footer}>
@@ -27,6 +37,6 @@ export default function Home() {
           </span>
         </div>
       </footer>
-    </div>
+    </div >
   )
 }
